6c7518c206a3df5d4a6b7b44a30a99275f682b7a
Agung <indoside@live.nl>
2019-06-17 00:36:06 +0200

    story intro's done

--- js/javascript.js
+++ js/javascript.js
@@ -4,15 +4,33 @@
 
 var controller = new ScrollMagic.Controller();
 
-// define movement of panels
+// define GS
 
-var wipeAnimation2 = new TimelineMax()
+var wipeAnimation1 = new TimelineMax()
     .set('.story-img-1, .story-bg-1', { transformOrigin: 'right center'})
     .staggerFromTo(".story-img-1, .story-bg-1", 1, {opacity: 0, x: "75%" }, {opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
 
-var wipeAnimation3 = new TimelineMax()
+var wipeAnimation2 = new TimelineMax()
     .set('.story-img-2, .story-bg-2', { transformOrigin: 'right center' })
     .staggerFromTo(".story-img-2, .story-bg-2", 1, { opacity: 0, x: "75%" }, { opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
+
+var wipeAnimation3 = new TimelineMax()
+    .set('.story-img-3, .story-bg-3', { transformOrigin: 'right center' })
+    .staggerFromTo(".story-img-3, .story-bg-3", 1, { opacity: 0, x: "75%" }, { opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
+
+var wipeAnimation4 = new TimelineMax()
+    .set('.story-img-4, .story-bg-4', { transformOrigin: 'right center' })
+    .staggerFromTo(".story-img-4, .story-bg-4", 1, { opacity: 0, x: "75%" }, { opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
+
+var wipeAnimation5 = new TimelineMax()
+    .set('.story-img-5, .story-bg-5', { transformOrigin: 'right center' })
+    .staggerFromTo(".story-img-5, .story-bg-5", 1, { opacity: 0, x: "75%" }, { opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
+
+var wipeAnimation6 = new TimelineMax()
+    .set('.story-img-6, .story-bg-6', { transformOrigin: 'right center' })
+    .staggerFromTo(".story-img-6, .story-bg-6", 1, { opacity: 0, x: "75%" }, { opacity: 1, x: "0%", ease: Power1.easeOut }, .2);
+
+
 
 var infinityMove = new TimelineMax()
     .fromTo("div.intro-container .panel h2", 1, { y: "-70%" } ,{ y: "50%", ease: Linear.easeNone})
@@ -59,6 +77,57 @@ $('.start-story h2').each(function(){
         .addTo(controller);
 });
 
+// start of story slide animation
+
+var startStory1 = new ScrollMagic.Scene({
+    triggerElement: ".story1-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation1)
+    // .addIndicators({
+    //     name: 'check wipeAnimation1',
+    //     colorStart: 'green',
+    //     colorEnd: 'red'
+    // })
+    .addTo(controller);
+
+var startStory2 = new ScrollMagic.Scene({
+    triggerElement: ".story2-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation2)
+    .addTo(controller);
+
+var startStory3 = new ScrollMagic.Scene({
+    triggerElement: ".story3-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation3)
+    .addTo(controller);
+
+var startStory4 = new ScrollMagic.Scene({
+    triggerElement: ".story4-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation4)
+    .addTo(controller);
+
+var startStory5 = new ScrollMagic.Scene({
+    triggerElement: ".story5-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation5)
+    .addTo(controller);
+
+var startStory6 = new ScrollMagic.Scene({
+    triggerElement: ".story6-container",
+    duration: "0"
+})
+    .setTween(wipeAnimation6)
+    .addTo(controller);
+
+// end story slide animation
+
 var infinityScroll = new ScrollMagic.Scene({
     triggerElement: "div.intro-container .panel",
     triggerHook: 'onCenter',
@@ -72,18 +141,6 @@ var infinityScroll = new ScrollMagic.Scene({
 })
 .addTo(controller);
 
-// var scrollStartStory = new ScrollMagic.Scene({
-//     triggerElement: ".start-story h2",
-//     triggerHook: 'onCenter',
-//     duration: "100%"
-// })
-//     .setTween(startStoryScroll)
-//     .addIndicators({
-//         name: 'check startStoryScroll ',
-//         colorStart: 'yellow',
-//         colorEnd: 'red'
-//     })
-//     .addTo(controller);
 
 var byeText = new ScrollMagic.Scene({
     triggerElement: "div.intro-container .panel p",
@@ -113,20 +170,6 @@ var staggerComicPanels = new ScrollMagic.Scene({
     .addTo(controller);
 
 
-var startStory1 = new ScrollMagic.Scene({
-    triggerElement: ".story1-container",
-    // triggerHook: "onLeave",
-    duration: "0"
-})
-    // .setPin("#pinContainer-2")
-    .setTween(wipeAnimation2)
-    .addIndicators({
-        name: 'check scroll 2',
-        colorStart: 'green',
-        colorEnd: 'red'
-    })
-    .addTo(controller);
-
 
 var story1Images = new ScrollMagic.Scene({
     triggerElement: ".story1-content p",
@@ -155,17 +198,6 @@ var story1Text = new ScrollMagic.Scene({
     .addTo(controller);
   
 
-var startStory2 = new ScrollMagic.Scene({
-    triggerElement: ".story2-container",
-    duration: "0"
-})
-    .setTween(wipeAnimation3)
-    .addIndicators({
-        name: 'check wipeAnimation3',
-        colorStart: 'purple',
-        colorEnd: 'red'
-    })
-    .addTo(controller);
 
 var story2TextFade = new ScrollMagic.Scene({
     triggerElement: ".story2-content",
